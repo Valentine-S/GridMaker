8ac1ed9334db45c526a8925b39715941dbff4a83
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-04 21:03:38 -0500

    Created index, styles and script files and initialized  basic layout of buttons as well as button function stubs.
    ;

---
+++ script.js
@@ -0,0 +1,29 @@
+let numRows = 0;
+let numCols = 0;
+let colorSelected;
+
+/* Button Functions: */
+
+// Add Row
+function addRow() {}
+
+// Add Column
+function addColumn() {}
+
+// Remove Row
+function removeRow() {}
+
+// Remove Column
+function removeColumn() {}
+
+// Fill All Uncolored
+function fillAllUncolored() {}
+
+// Fill
+function fill() {}
+
+// Clear All
+function clearAll() {}
+
+// Select a Color (Changes glob var colorSelected)
+function selected() {}
