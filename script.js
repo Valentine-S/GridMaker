011a2689cc5377f66fe164a0d7e010c411ba1914
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-11 20:42:02 -0500

    Modified functions

--- script.js
+++ script.js
@@ -6,94 +6,29 @@ let colorSelected;
 
 // Add Row
 function addRow() {
-    var row = document.createElement('tr');
-    row.className = "row"
-    if (numCols == 0) {
-        let cell = document.createElement('td')
-        row.appendChild(cell)
-        numCols++
-    }
-    else {
-        for (let i = 0; i < numCols; i++) {
-            let cell = document.createElement('td')
-            row.appendChild(cell)
-        }
-    }
-    grid.appendChild(row)
-    numRows++
 }
 
 // Add Column
 function addColumn() {
-    if (numRows == 0) {
-        addRow()
-    }
-    else {
-        let rows = document.querySelectorAll('.row');
-        rows.forEach(function (row) {
-            let col = document.createElement('td')
-            row.appendChild(col)
-        })
-        numCols++;
-    }
 }
-
 // Remove Row
 function removeRow() {
-    if (numRows > 0) {
-        grid.lastElementChild.remove()
-        numRows--;
-    }
-    if (numRows == 0) {
-        numCols = 0
-    }
-
 }
 
 // Remove Column
 function removeColumn() {
-    if (numCols > 0) {
-        let rows = document.querySelectorAll('.row')
-        rows.forEach(function (row) {
-            row.lastElementChild.remove()
-        })
-        numCols--;
-    }
-    if (numCols == 0) {
-        while (grid.firstChild) {
-            grid.removeChild(grid.lastChild)
-        }
-        numRows = 0
-    }
-
 }
 
 // Fill All Uncolored
 function fillAllUncolored() {
-    var cells = grid.getElementsByTagName("td")
-    for (let i = 0; i < cells.length; i++) {
-        currentStyle = getComputedStyle(cells[i])
-        currentColor = currentStyle.backgroundColor
-        if (currentColor == "rgb(255, 255, 255)") {
-            cells[i].style.backgroundColor = colorSelected
-        }
-    }
 }
 
 // Fill
 function fill() {
-    var cells = grid.getElementsByTagName("td")
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].style.backgroundColor = colorSelected
-    }
 }
 
 // Clear All
 function clearAll() {
-    var cells = grid.getElementsByTagName("td")
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].style.backgroundColor = "white"
-    }
 }
 
 // Select a Color (Changes glob var colorSelected)
@@ -112,19 +47,19 @@ function selected() {
     else if (color == "Blue") {
         colorSelected = "#0000FF";
     }
-    //changeCell(); //where to put so this is always active ? 
 }
 
 
 // Change Cell Color(change color By clicking)
 function changeCell() {
-    var clicked = document.getElementById("td").addEventListener("click");
+    var clicked = onClick();
     if (clicked == true) {
-        alert("box clicked")
         document.getElementById("box").background = colorSelected;
     }
     // mouse clicks on cell change to colorSelecetd value
 }
+/*
 $("#box").animate().css({
     background: colorSelected
 }, 2500);
+*/
